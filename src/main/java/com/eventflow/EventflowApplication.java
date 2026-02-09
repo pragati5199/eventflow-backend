b58f00ec2bc2f126a04e53713e502e9ae13f8746
Prashant Saxena <your-email@gmail.com>
2026-02-09 10:33:49 +0530

    Initial Spring boot project setup

---
+++ src/main/java/com/eventflow/EventflowApplication.java
@@ -0,0 +1,13 @@
+package com.eventflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventflow/eventflow/EventflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventflow.eventflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
